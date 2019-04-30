6c341851026c82ebd91ded1c7c3237b72c52966e
Domante Stirbyte <domante.stirbyte@gmail.com>
2019-04-30 11:58:57 +0200

    Updates

---
+++ precache-manifest.36850c38be73a5edafc7c92582427b31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef7abc3acf8d2d0d9d22",
+    "url": "/Recipe-App-with-React/static/js/runtime~main.ee11e00a.js"
+  },
+  {
+    "revision": "1ad722b3840f7c2415c5",
+    "url": "/Recipe-App-with-React/static/js/main.c7682f3e.chunk.js"
+  },
+  {
+    "revision": "df155a8e9073ae43fd1c",
+    "url": "/Recipe-App-with-React/static/js/2.9e03ac05.chunk.js"
+  },
+  {
+    "revision": "1ad722b3840f7c2415c5",
+    "url": "/Recipe-App-with-React/static/css/main.9dda97d8.chunk.css"
+  },
+  {
+    "revision": "d91412118e34cc15256440bdb01b97bd",
+    "url": "/Recipe-App-with-React/index.html"
+  }
+];
